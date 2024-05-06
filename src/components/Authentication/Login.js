6b07491658ff4bed8412ca523169b8137642df73
kedarlin <kedarkamaf@gmail.com>
2024-05-06 21:43:17 +0530

    initial commit

---
+++ src/components/Authentication/Login.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { changeView } from '@/redux/slices/authModalSlice';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/firebaseClient';
+import { FIREBASE_ERRORS } from '@/firebase/errors';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [eror, setEror] = useState('');
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const dispatch = useDispatch();
+    const [signInWithGoogle, userCred] = useSignInWithGoogle(auth);
+    const [
+        signInWithEmailAndPassword,
+        user,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const handleSignupClick = () => {
+        dispatch(changeView('signup'));
+    };
+
+    const handleEmailLoginClick = () => {
+        setDialogOpen(true);
+    };
+
+    const handleCloseDialog = () => {
+        setDialogOpen(false);
+    };
+
+    const handleSubmit = () => {
+        if(password==='' || email==='') setEror("Enter the username and password");
+        if(eror) setEror('');
+        console.log(email, password);
+        signInWithEmailAndPassword(email, password);
+        console.log(error);
+    };
+
+    return (
+        <div className='auth-box'>
+            <div className='auth-title'>Login</div>
+            <div className='auth-title-desc'>Choose a Login method</div>
+            <div className='auth-buttons'>
+                <div className='auth-button' onClick={() => signInWithGoogle()}>
+                    <img src='/Assets/google.png' alt='auth-google-icon' className='auth-button-icons' />
+                    <div className='auth-button-name'>Log In with Google</div>
+                </div>
+                <div className='auth-button' onClick={handleEmailLoginClick}>
+                    <img src='/Assets/mail.png' alt='auth-mail-icon' className='auth-button-icons' />
+                    <div className='auth-button-name'>Log In with Email</div>
+                </div>
+            </div>
+            <div className="link-container">
+                <span className="link-text">New Here?  </span>
+                <span
+                    className="link-texts"
+                    onClick={handleSignupClick}
+                >
+                    Sign Up
+                </span>
+            </div>
+
+            {dialogOpen && (
+                <div className="dialog-overlay" onClick={handleCloseDialog}>
+                    <div className="dialog-content" onClick={(e) => e.stopPropagation()}>
+                        <div className='auth-title' style={{ paddingBottom: "30px" }}>Login</div>
+                        <input
+                            className='auth-input'
+                            type="email"
+                            placeholder="Enter your email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            className='auth-input'
+                            type="password"
+                            placeholder="Enter your password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <div className="auth-error-message">{eror || ( error && FIREBASE_ERRORS[error.message]) }</div>
+                        <button onClick={handleSubmit} className='auth-submit'>Login</button>
+                        <div className="link-container">
+                            <span className="link-text">Don't have an account?  </span>
+                            <span
+                                className="link-texts"
+                                onClick={handleSignupClick}
+                            >
+                                Sign Up
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Login;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import "@/styles/globals.css";
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import '../styles/globals.css';
+import 'dotenv/config';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
